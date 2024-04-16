a18738012d6ca67b61c538af123d796bbd9f8cdd
Lars Knoke <larz.knoke@gmail.com>
2024-04-16 23:10:39 +0200

    init

---
+++ src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+
+function Header() {
+  return (
+    <div className="p-4">
+      <div>
+        <Bars3Icon className="w-8" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="http://10.10.10.3:8082/socket.io/socket.io.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
