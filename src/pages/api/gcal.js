1110de50693facdde12fcf449b97f4b17692ece1
Lars Knoke <l.knoke@colorplus.de>
2024-05-23 13:46:55 +0200

    gcal

--- src/pages/api/gcal.js
+++ src/pages/api/gcal.js
@@ -12,8 +12,8 @@ export default async (req, res) => {
     // +++++++++++++++++++++
     const session = await getServerSession(req, res, authOptions);
     const token = await getToken({ req });
-    console.log("session", session);
-    console.log("token", token);
+    // console.log("session", session);
+    // console.log("token", token);
 
     if (!session) {
       return res.status(401).json({ error: "No Session Active" });
@@ -40,25 +40,37 @@ export default async (req, res) => {
       session.user.accessToken = newAccessToken;
     }
 
-    console.log("oauth2Client", oauth2Client);
+    // console.log("oauth2Client", oauth2Client);
 
     // +++++++++++++++++++++
     // GET CALENDAR EVENTS
     // +++++++++++++++++++++
 
     const { cals } = req.query;
-    const calsArr = cals?.split(",") || [];
+    const calsParams = cals?.split(",") || [];
 
-    const allEvents = [];
+    let allIDs = [];
     const alleCalIds = [
       "15o5sfjde220dar98njal5m3u4@group.calendar.google.com",
       "c_4f27396d8df958dc4c4e421a49473e0e7571bd176ba15d9be1a97b0f70dbb5e4@group.calendar.google.com",
     ];
+    const jonasCalIds = [
+      "kfjresb61dnbrqo4a7gtr1aaq53vnh58@import.calendar.google.com",
+    ];
+
+    if (calsParams.includes("alle")) {
+      allIDs = allIDs.concat(alleCalIds);
+    }
+    if (calsParams.includes("jk")) {
+      allIDs = allIDs.concat(jonasCalIds);
+    }
+
+    const uniqIds = [...new Set(allIDs)];
 
     const calendar = google.calendar({ version: "v3", auth: oauth2Client });
 
     const allPromiseEvents = await Promise.all(
-      alleCalIds.map(async (calID) => {
+      uniqIds.map(async (calID) => {
         const calData = await calendar.events.list({
           calendarId: calID,
           timeMin: new Date().toISOString(),
