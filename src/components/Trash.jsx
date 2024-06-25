0a9426d06f58402dd2f0b633c1ebec4b9bcc084f
Lars Knoke <larz.knoke@gmail.com>
2024-06-25 11:04:16 +0200

    Trash 0 und 1 Tage

--- src/components/Trash.jsx
+++ src/components/Trash.jsx
@@ -2,30 +2,41 @@ import { Text, HStack, VStack } from "@chakra-ui/react";
 import { Trash as TrashIcon } from "@phosphor-icons/react";
 
 function Trash({ rest, bio, gelb, papier }) {
+  function trashDays(trash) {
+    switch (trash) {
+      case 0:
+        return "Heute";
+      case 1:
+        return "Morgen";
+      default:
+        return `${trash} Tage`;
+    }
+  }
+
   return (
     <HStack alignSelf={"center"} gap={24}>
       <VStack gap={2}>
         <TrashIcon size={32} className=" text-gray-600" />
         <Text as={"b"} fontSize={"3xl"}>
-          {rest == 1 ? "Morgen" : `${rest} Tage`}
+          {trashDays(rest)}
         </Text>
       </VStack>
       <VStack gap={2}>
         <TrashIcon size={32} className=" text-green-700" />
         <Text as={"b"} fontSize={"3xl"}>
-          {bio == 1 ? "Morgen" : `${bio} Tage`}
+          {trashDays(bio)}
         </Text>
       </VStack>
       <VStack gap={2}>
         <TrashIcon size={32} className=" text-yellow-400" />
         <Text as={"b"} fontSize={"3xl"}>
-          {gelb == 1 ? "Morgen" : `${gelb} Tage`}
+          {trashDays(gelb)}
         </Text>
       </VStack>
       <VStack gap={2}>
         <TrashIcon size={32} className=" text-blue-700" />
         <Text as={"b"} fontSize={"3xl"}>
-          {papier == 1 ? "Morgen" : `${papier} Tage`}
+          {trashDays(papier)}
         </Text>
       </VStack>
     </HStack>
