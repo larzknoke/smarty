a385ca8697e988931dce0ca24ed2caf0dcb5a3ea
Lars Knoke <larz.knoke@gmail.com>
2025-03-30 22:28:26 +0200

    EVCC

--- src/components/PvData.jsx
+++ src/components/PvData.jsx
@@ -2,6 +2,7 @@ import { socketConnection, connectSocket } from "@/lib/socketConnection";
 import { useState, useEffect } from "react";
 import { EMValue, makeNegativeNumberZero } from "@/lib/utils";
 import {
+  Button,
   Text,
   Flex,
   HStack,
@@ -10,14 +11,30 @@ import {
   Image,
   Divider,
   Switch,
+  RadioGroup,
+  Radio,
+  ButtonGroup,
 } from "@chakra-ui/react";
 import { Car } from "@phosphor-icons/react";
+
+let buttonStylePvMode = {
+  textTransform: "uppercase",
+  colorScheme: "teal",
+  w: "100%",
+  fontSize: "40px",
+  height: "80px",
+  px: "20px",
+};
 
 function PoolData() {
   const states = [
     "modbus.0.inputRegisters.30775_Leistung",
     "shelly.0.SHEM-3#E8DB84D68ECE#1.Total.InstantPower",
     "modbus.0.inputRegisters.30535_Tagesertrag",
+    "evcc.0.loadpoint.1.status.connected",
+    "evcc.0.loadpoint.1.status.charging",
+    "evcc.0.loadpoint.1.status.chargePower",
+    "evcc.0.loadpoint.1.status.mode",
   ];
 
   const [socket, setSocket] = useState();
@@ -47,6 +64,25 @@ function PoolData() {
 
   function handleToggle(val) {
     socket?.setState(val, !values[val]);
+  }
+
+  function setPvMode(val) {
+    // socket?.setState(val, !values[val]);
+    console.log("PV Mode", val);
+    switch (val) {
+      case "off":
+        socket?.setState("evcc.0.loadpoint.1.control.off", 1);
+        break;
+      case "pv":
+        socket?.setState("evcc.0.loadpoint.1.control.pv", 1);
+        break;
+      case "minpv":
+        socket?.setState("evcc.0.loadpoint.1.control.min", 1);
+        break;
+      case "now":
+        socket?.setState("evcc.0.loadpoint.1.control.now", 1);
+        break;
+    }
   }
 
   return (
@@ -95,13 +131,21 @@ function PoolData() {
           <Heading size={"xl"}>Tagesverbrauch</Heading>
         </VStack>
       </HStack>
-      <VStack alignItems={"flex-start"} opacity={0.3}>
+      <VStack alignItems={"flex-start"}>
         <HStack justifyContent={"space-between"} w={"100%"}>
           <Heading size={"2xl"} color={"gray.500"}>
             Auto
           </Heading>
           <HStack>
-            <Car size={54} className=" text-gray-400" />
+            {values["evcc.0.loadpoint.1.status.connected"] ? (
+              <Heading size={"xl"} color={"green.500"}>
+                verbunden
+              </Heading>
+            ) : (
+              <Heading size={"xl"} color={"red.500"}>
+                nicht verbunden
+              </Heading>
+            )}
           </HStack>
         </HStack>
         <Divider my={6} size="xl" sx={{ borderBottomWidth: "4px" }} />
@@ -112,13 +156,25 @@ function PoolData() {
             alignItems={"flex-start"}
           >
             <VStack>
-              <Heading size={"3xl"} color={"green.500"}>
-                Lädt
-              </Heading>
+              {values["evcc.0.loadpoint.1.status.charging"] ? (
+                <Heading size={"3xl"} color={"green.500"}>
+                  Lädt
+                </Heading>
+              ) : (
+                <Heading size={"3xl"} color={"red.500"}>
+                  Lädt nicht
+                </Heading>
+              )}{" "}
               <Heading size={"xl"}>Status</Heading>
             </VStack>
             <VStack>
-              <Heading size={"3xl"}>4 kWh</Heading>
+              <Heading size={"3xl"}>
+                {" "}
+                {(
+                  values["evcc.0.loadpoint.1.status.chargePower"] / 1000
+                ).toFixed(1)}{" "}
+                kW
+              </Heading>
               <Heading size={"xl"}>Leistung</Heading>
             </VStack>
             <VStack>
@@ -126,7 +182,80 @@ function PoolData() {
               <Heading size={"xl"}>Batterie</Heading>
             </VStack>
           </HStack>
-          <HStack
+          <HStack w={"100%"}>
+            <VStack gap={8} w={"100%"} my={16}>
+              <ButtonGroup
+                spacing="6"
+                justifyContent={"space-between"}
+                w={"100%"}
+              >
+                <Button
+                  {...buttonStylePvMode}
+                  variant={
+                    values["evcc.0.loadpoint.1.status.mode"] === "off"
+                      ? "solid"
+                      : "outline"
+                  }
+                  onClick={() => setPvMode("off")}
+                >
+                  Aus
+                </Button>
+                <Button
+                  {...buttonStylePvMode}
+                  variant={
+                    values["evcc.0.loadpoint.1.status.mode"] === "pv"
+                      ? "solid"
+                      : "outline"
+                  }
+                  onClick={() => setPvMode("pv")}
+                >
+                  PV
+                </Button>
+                <Button
+                  {...buttonStylePvMode}
+                  variant={
+                    values["evcc.0.loadpoint.1.status.mode"] === "minpv"
+                      ? "solid"
+                      : "outline"
+                  }
+                  onClick={() => setPvMode("minpv")}
+                >
+                  Min+PV
+                </Button>
+                <Button
+                  {...buttonStylePvMode}
+                  variant={
+                    values["evcc.0.loadpoint.1.status.mode"] === "now"
+                      ? "solid"
+                      : "outline"
+                  }
+                  onClick={() => setPvMode("now")}
+                >
+                  Sofort
+                </Button>
+              </ButtonGroup>
+              <Heading size={"xl"}>Lade-Modus</Heading>
+            </VStack>
+          </HStack>
+          {/* <HStack>
+            <RadioGroup onChange={setPvMode}>
+              <HStack>
+                <Radio size="lg" value="aus">
+                  Aus
+                </Radio>
+                <Radio size="lg" value="pv">
+                  PV
+                </Radio>
+                <Radio size="lg" value="minpv">
+                  Min+PV
+                </Radio>
+                <Radio size="lg" value="schnell">
+                  Schnell
+                </Radio>
+              </HStack>
+            </RadioGroup>
+          </HStack> */}
+          {/* <HStack
             justifyContent={"space-between"}
             w={"100%"}
             alignItems={"flex-start"}
@@ -148,7 +277,7 @@ function PoolData() {
                 tägl. 8.00-16.00 Uhr
               </Text>
             </VStack>
-          </HStack>
+          </HStack> */}
         </VStack>
       </VStack>
     </>
